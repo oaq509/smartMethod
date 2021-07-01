6f6e42ca6ca1f4991e61b3a6f567df5b4abe287a
oaq509 <85634034+oaq509@users.noreply.github.com>
2021-07-01 22:33:05 +0300

    Update db.php

--- task 1 & 2 robot control panel motor + motion/db.php
+++ task 1 & 2 robot control panel motor + motion/db.php
@@ -7,8 +7,6 @@
 //$dbname="";
 
 if($cont=new MySQLi(localhost,root,1234567,'robot'))
-//if($cont=new MySQLi("sql100.eb2a.com","eb2a_27344183","1234567","eb2a_27344183_cv"))
-//if($cont=new mysqli($host,$user,$pass,$dbname))
 
 {//echo'succsed '. '<br/><br/>';
 
